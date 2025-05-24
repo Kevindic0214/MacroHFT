8e27c324c810fecb7ce4fc1d746c874a87001147
Kevindic0214 <hsiehk0214@gmail.com>
2025-05-24 13:16:15 +0000

    Fix device assignments in low_level.sh for correct GPU usage

--- scripts/low_level.sh
+++ scripts/low_level.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+mkdir -p ./logs/low_level/ETHUSDT/
+
 nohup python -u RL/agent/low_level.py --alpha 1 --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' \
     --label label_1 >./logs/low_level/ETHUSDT/slope_1.log 2>&1 &
 
@@ -7,13 +11,13 @@ nohup python -u RL/agent/low_level.py --alpha 4 --clf 'slope' --dataset 'ETHUSDT
 nohup python -u RL/agent/low_level.py --alpha 0 --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:2' \
     --label label_3 >./logs/low_level/ETHUSDT/slope_3.log 2>&1 &
 
-nohup python -u RL/agent/low_level.py --alpha 4 --clf 'vol' --dataset 'ETHUSDT' --device 'cuda:0' \
+nohup python -u RL/agent/low_level.py --alpha 4 --clf 'vol' --dataset 'ETHUSDT' --device 'cuda:3' \
     --label label_1 >./logs/low_level/ETHUSDT/vol_1.log 2>&1 &
 
-nohup python -u RL/agent/low_level.py --alpha 1 --clf 'vol' --dataset 'ETHUSDT' --device 'cuda:1' \
+nohup python -u RL/agent/low_level.py --alpha 1 --clf 'vol' --dataset 'ETHUSDT' --device 'cuda:4' \
     --label label_2 >./logs/low_level/ETHUSDT/vol_2.log 2>&1 &
 
-nohup python -u RL/agent/low_level.py --alpha 1 --clf 'vol' --dataset 'ETHUSDT' --device 'cuda:2' \
+nohup python -u RL/agent/low_level.py --alpha 1 --clf 'vol' --dataset 'ETHUSDT' --device 'cuda:5' \
     --label label_3 >./logs/low_level/ETHUSDT/vol_3.log 2>&1 &
 
 
