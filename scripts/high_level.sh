10fc5d591baea626b6c0f672543117cd13bdf7cb
Kevindic0214 <hsiehk0214@gmail.com>
2025-05-29 03:33:38 +0000

    Update logging redirection in high_level.sh and low_level.sh to append logs

--- scripts/high_level.sh
+++ scripts/high_level.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./logs/high_level/
-
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 nohup python -u RL/agent/high_level.py --dataset 'ETHUSDT' --device 'cuda:6' \
-    >./logs/high_level/ETHUSDT.log 2>&1 &
+    >>./logs/high_level/ETHUSDT_$TIMESTAMP.log 2>&1 &
